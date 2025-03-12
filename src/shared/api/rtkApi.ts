9db4f758a09ebbf94a3657464a7b593de09a0ef0
venomsurgut <lexus987321@gmail.com>
2025-03-12 23:59:46 +0100

    fix: api

--- src/shared/api/rtkApi.ts
+++ src/shared/api/rtkApi.ts
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import {site} from "../const/const";
+import {domen} from "../const/const";
 // import { TOKEN_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 export const api = createApi({
 	reducerPath: 'api',
 	baseQuery: fetchBaseQuery({
-		baseUrl: `${site}/api/`,
+		baseUrl: `${domen}/api/`,
 	}),
 	endpoints: () => ({}),
 });
--- src/shared/const/const.ts
+++ src/shared/const/const.ts
@@ -1,4 +1,6 @@
 // @ts-ignore
 export const site = import.meta.env.VITE_SITE ?? 'https://lk.medanketa.com'
+// @ts-ignore
+export const domen = import.meta.env.VITE_DOMEN ?? 'https://medanketa.com'
 export const sile_login = site
 export const sile_register = `${site}/register`
